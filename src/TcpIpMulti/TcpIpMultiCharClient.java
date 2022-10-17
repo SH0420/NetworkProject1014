b22f9c3f61964d2f65d26e005e84d99bf64ac96e
SH0420 <hantjdgh01@naver.com>
2022-10-17 12:19:37 +0900

    1017

--- src/TcpIpMulti/TcpIpMultiCharClient.java
+++ src/TcpIpMulti/TcpIpMultiCharClient.java
@@ -1,3 +1,4 @@
+package TcpIpMulti;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -8,11 +9,17 @@ public class TcpIpMultiCharClient {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("닉네임 입력:");
-		String nikcName = sc.next();
+		String nickName = sc.next();
 		
 		try {
-			Socket socket = new Socket("192.168.0.241",7777);
+			Socket socket = new Socket("192.168.0.224",7777);
 			System.out.println("서버에 연결됨");
+			Thread sender = new Thread(new ClientSender(socket,nickName));
+			Thread receiver = new Thread(new ClientReceiver(socket));
+			
+			sender.start();
+			receiver.start();
+			
 		} catch (UnknownHostException e) {
 		
 			e.printStackTrace();
